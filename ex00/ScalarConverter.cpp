2d136b8f0eccd43c6cd229a10b9f72ba376ea232
lde-mich <luca01112003@gmail.com>
2024-02-21 20:27:38 +0100

    upload-feb-21

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: lde-mich <lde-mich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 19:31:31 by lde-mich          #+#    #+#             */
-/*   Updated: 2024/02/21 20:03:38 by lde-mich         ###   ########.fr       */
+/*   Updated: 2024/02/21 20:16:39 by lde-mich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ ScalarConverter &ScalarConverter::operator=(ScalarConverter const &obj)
 
 void ScalarConverter::convert(std::string value)
 {
-
+	std::cout << value << std::endl;
 }
 
 
