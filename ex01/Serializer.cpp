e048ce19af3bd86c2ab82dae65a4b6d9dd857ae2
lde-mich <luca01112003@gmail.com>
2024-02-27 14:08:52 +0100

    upload-feb-27

--- ex01/Serializer.cpp
+++ ex01/Serializer.cpp
@@ -6,7 +6,7 @@
 /*   By: lde-mich <lde-mich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 15:28:50 by lde-mich          #+#    #+#             */
-/*   Updated: 2024/02/23 15:32:16 by lde-mich         ###   ########.fr       */
+/*   Updated: 2024/02/27 14:06:21 by lde-mich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,7 @@ Serializer::Serializer()
 
 Serializer::Serializer(Serializer const &obj)
 {
-	(void) obj;
-	return (*this);
+	(*this) = obj;
 }
 
 Serializer::~Serializer()
@@ -30,14 +29,29 @@ Serializer::~Serializer()
 }
 
 
+Serializer Serializer::operator=(Serializer const &obj)
+{
+	(void) obj;
+
+	return (*this);
+}
+
 
 uintptr_t Serializer::serialize(Data* ptr)
 {
+	uintptr_t raw;
+	raw = reinterpret_cast<uintptr_t>(ptr);
+
+	return (raw);
 }
 
 
 Data* Serializer::deserialize(uintptr_t raw)
 {
+	Data *new_ptr;
+	new_ptr = reinterpret_cast<Data *>(raw);
+
+	return (new_ptr);
 }
 
 
