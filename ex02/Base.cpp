95da8e49c27d6554b69f81b3b34a67dab694136f
lde-mich <luca01112003@gmail.com>
2024-02-28 12:39:00 +0100

    upload-feb-28

--- ex02/Base.cpp
+++ ex02/Base.cpp
@@ -6,7 +6,7 @@
 /*   By: lde-mich <lde-mich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 14:32:20 by lde-mich          #+#    #+#             */
-/*   Updated: 2024/02/28 12:30:31 by lde-mich         ###   ########.fr       */
+/*   Updated: 2024/02/28 12:37:54 by lde-mich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,8 +78,6 @@ void identify(Base &p)
 			std::cout << "&B" << std::endl;
 		else if(dynamic_cast<C*>(&p))
 			std::cout << "&C" << std::endl;
-		else
-			std::cout << "Type not found" << std::endl;
 	}
 	catch (std::exception &e)
 	{
